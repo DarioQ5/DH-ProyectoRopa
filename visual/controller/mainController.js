5cd618bea100be405b650931c97dbe0c7e4db3a8
maite-aramayo <maitearamayo90@gmail.com>
2023-08-21 21:42:55 -0300

    agregue base de datos

--- visual/controller/mainController.js
+++ visual/controller/mainController.js
@@ -1,63 +1,10 @@
 const path = require('path');
 
+const productos = require('../dataBase/productos')
+
 const mainController = {
     renderHome: (req, res) => {
-        let productos = [
-        {
-            name: "Camisa de vestir para hombre",
-            description: "Camisa elegante de manga larga para ocasiones formales.",
-            image: "imagen_camisa_hombre.jpg",
-            category: "Ropa masculina",
-            colores: ["Blanco", "Azul", "Negro"],
-            tallas: ["S", "M", "L", "XL"],
-            price: 39.99
-            },
-            {
-            name: "Vestido de noche para mujer",
-            description: "Vestido elegante perfecto para eventos nocturnos.",
-            image: "imagen_vestido_mujer.jpg",
-            category: "Ropa femenina",
-            colores: ["Rojo", "Negro", "Dorado"],
-            tallas: ["XS", "S", "M", "L"],
-            price: 49.99
-            },
-            {
-            name: "Jeans ajustados para hombre",
-            description: "Jeans modernos y cómodos para uso diario.",
-            image: "imagen_jeans_hombre.jpg",
-            category: "Ropa masculina",
-            colores: ["Azul oscuro", "Negro"],
-            tallas: ["30x32", "32x32", "34x32", "36x32"],
-            price: 29.99
-            },
-            {
-            name: "Blusa casual para mujer",
-            description: "Blusa ligera y cómoda, ideal para el día a día.",
-            image: "imagen_blusa_mujer.jpg",
-            category: "Ropa femenina",
-            colores: ["Blanco", "Azul claro", "Rosa"],
-            tallas: ["S", "M", "L"],
-            price: 24.99
-            },
-            {
-            name: "Blusa casual para mujer",
-            description: "Blusa ligera y cómoda, ideal para el día a día.",
-            image: "imagen_blusa_mujer.jpg",
-            category: "Ropa femenina",
-            colores: ["Blanco", "Azul claro", "Rosa"],
-            tallas: ["S", "M", "L"],
-            price: 24.99
-            },
-            {
-            name: "Blusa casual para mujer",
-            description: "Blusa ligera y cómoda, ideal para el día a día.",
-            image: "imagen_blusa_mujer.jpg",
-            category: "Ropa femenina",
-            colores: ["Blanco", "Azul claro", "Rosa"],
-            tallas: ["S", "M", "L"],
-            price: 24.99
-            }
-        ];
+       
         res.render('index', {'productos': productos});
     },
     renderLogin: (req, res) => {
